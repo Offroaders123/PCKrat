9cbe695f1fa763a9be2fcd64fb6fbc2b57eead14
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2024-03-17 23:56:34 -0700

    Interface Shaper + Reader Class
    Ok, I think I might like the reader class setup afterall, not completely sure yet. If there's mutatable state, is it more understandable to define it with a class context around it? Passing state values around between individual function implementations feels a bit off to me, at least in terms of parsing a file. Maybe this will change for me though, I really like functional programming for simple, non-stateful things, and I think using classes for stateful ones seems to feel kind of nice. Both have their downsides though, classes are less modular in use with other code, and pure functional things feel a bit cumbersome. Maybe it's just JS's lack of pass-by-reference ability, I think that could help a bunch with not needing to use classes for mutable state.
    
    I think my brain is on a bit of a break from file parsing as of lately, might try out some other coding projects to see where I'm at again. Coding in general has been on a bit of a standstill for me the last few days, I think being busy has to do with that.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,54 @@
-export function read(data: Uint8Array) {
-  const view = new DataView(data.buffer, data.byteOffset, data.byteLength);
-  let offset: number = 0;
+export function read(data: Uint8Array): PCK {
+  return new PCKReader(data).read();
+}
 
-  const type: number = view.getUint32((offset += 4) - 4);
-  console.log(type);
+export interface PCK {
+  type: number;
+  lookupTable: LookupTable[];
+  fileEntries: FileEntry[];
+  fileContents: FileContent[];
+}
 
-  const lookupTableCount: number = view.getUint32((offset += 4) - 4);
-  const lookupTable: number[] = Array(lookupTableCount);
+export interface LookupTable {
+  index: number;
+  value: string;
+}
 
-  for (let i = 0; i < lookupTable.length; i++){
-    const index: number = view.getUint32((offset += 4) - 4);
-    const valueLength: number = view.getUint16((offset += 2) - 2);
-    console.log(valueLength);
-    // const value: string = ;
-    offset += valueLength;
-    lookupTable[i] = index;
+export interface FileEntry {
+  fileSize: number;
+  fileType: FileType;
+  fileName: string;
+}
+
+export interface FileContent {}
+
+export enum FileType {
+  Skin = 0,
+  Cape,
+  Texture,
+  UIData,
+  Info,
+  TexturePackInfo,
+  Localisation,
+  GameRules,
+  Audio,
+  ColourTable,
+  GameRulesHeader,
+  SkinData,
+  Models,
+  Behaviours,
+  Material
+}
+
+export class PCKReader {
+  public readonly data: Uint8Array;
+  private readonly view: DataView;
+  private offset: number = 0;
+
+  constructor(data: Uint8Array) {
+    this.data = data;
+    this.view = new DataView(this.data.buffer, this.data.byteOffset, this.data.byteLength);
   }
-  console.log(lookupTable);
 
-  const entriesCount: number = view.getUint32((offset += 4) - 4);
-  const entries: [] = Array(entriesCount);
-  console.log(entries);
+  read() {}
 }
